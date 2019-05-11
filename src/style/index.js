66ef67d10e01b5f101e3079c28813b76e08aa92e
Veneciac <veneciacalista00@gmail.com>
2019-05-11 22:41:28 +0700

    add color and package

---
+++ src/style/index.js
@@ -0,0 +1,22 @@
+//NOTES
+// X => EXTRA
+// L => LIGHT 
+// D => DARK 
+// S => SHADOW 
+// N => NEON
+
+export const color = {
+    white          : '#fff',
+    blueGreyXL     : 'rgb(246, 247, 249)',
+    greyXL         : 'rgb(245, 245, 245)',
+    greyL          : 'rgb(219, 219, 219)',
+    grey           : 'rgb(196, 196, 196)',
+    greyD          : 'rgb(85, 85, 86)',
+    yellow         : 'rgb(255, 190, 30)',
+    yellowS        : 'rgba(239, 171, 2, 0.6)',
+    black          : 'rgb(30, 30, 30)',
+    blackL         : 'rgb(50, 50, 50)',
+    tealDS         : 'rgba(2,128,144, 0.6)',
+    blueN          : 'rgb(63,142,252)',
+    
+}
